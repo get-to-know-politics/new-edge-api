413b0e2d64a9deef1d1869a04c33ca83a91f62af
Hannah Werman <hanna.w28@gmail.com>
2020-04-13 17:52:26 -0400

    more heroku db config

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -20,7 +20,7 @@ module.exports = {
     // "password": process.env.DB_PASS,
     // "database": process.env.DB_NAME,
     // "host": process.env.DB_HOST,
-    // "dialect": "postgres",
+    "dialect": "postgres",
     "use_env_variable": "DATABASE_URL"
   }
 }
